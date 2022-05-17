7035106dc9329617af2969947a4e1e485094ffc7
emilio.sagre@gmail.com <54941919+emiliosagre@users.noreply.github.com>
2022-05-17 00:37:53 -0700

    import reviewed dataset

--- DANA4830 - A1_Emilio, Steven.R
+++ DANA4830 - A1_Emilio, Steven.R
@@ -1,2 +1,6 @@
 # DANA 4830 - A1
 # Author: Emilio Sagre, Steven Whang
+
+# Import dataset
+AP <- read.csv("https://raw.githubusercontent.com/emiliosagre/DANA4830_Git/main/APCleaned_v2.csv", header = T, row.names = 1)
+
